b4142e22fac2fa509ad4d4c279725ee1db4550c8
Alejandro <alejandro.parreno-saiz@capgemini.com>
2022-08-08 12:50:15 +0200

    create spring boot project

---
+++ src/main/java/dev/parresoft9/hotelbeds/HotelbedsApplication.java
@@ -0,0 +1,13 @@
+package dev.parresoft9.hotelbeds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelbedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelbedsApplication.class, args);
+	}
+
+}
